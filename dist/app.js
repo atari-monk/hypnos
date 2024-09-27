36306b6caf8a883ba9ac14519cdce7f6af587823
atari-monk <atari.monk1@gmail.com>
2024-09-27 22:17:08 +0200

    adventure day2

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript API!');
+});
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    console.log('New user:', newUser);
+    res.status(201).json({ message: 'User created', user: newUser });
 });
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript API!')
+})
+
+app.post('/users', (req, res) => {
+  const newUser = req.body
+  console.log('New user:', newUser)
+  res.status(201).json({ message: 'User created', user: newUser })
 })
 
 app.listen(port, () => {
