531cdaf90ec60416fb80195bd7cb1e3d28fc379f
atari-monk <atari.monk1@gmail.com>
2024-09-27 22:27:34 +0200

    adventure day 4

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,14 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import { IUser } from './../interfaces/IUser'
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.send('User route')
 })
 
-router.post('/', (req, res) => {
-  const newUser = req.body
+router.post('/', (req: Request, res: Response) => {
+  const newUser: IUser = req.body
   console.log('New user:', newUser)
   res.status(201).json({ message: 'User created', user: newUser })
 })
